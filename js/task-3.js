fb0d542abe0bb3974ea3e0f88425152ada4c62e3
AlinkaMalinkA05 <148655414+AlinkaMalinkA05@users.noreply.github.com>
2024-01-14 20:31:02 +0100

    changed task 3

--- js/task-3.js
+++ js/task-3.js
@@ -1,8 +1,5 @@
 function getElementWidth(content, padding, border) {
-    Number.parseFloat(content),
-        Number.parseFloat(padding),
-        Number.parseFloat(border)
-    return Number.parseFloat(content) + 2*(Number.parseFloat(padding) +Number.parseFloat(border)) 
+return Number.parseFloat(content) + 2 * (Number.parseFloat(padding) + Number.parseFloat(border)) 
 }
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
